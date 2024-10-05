ad18e3582ebeb60874f8b73cf6a47739ae52a9da
Adil-Sami <adilsami230@gmail.com>
2024-10-05 06:29:36 +0500

    input validation

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
 from fastapi import FastAPI, HTTPException
+from enum import Enum
+
+class BandGenreChoices(Enum):
+    ROCK = 'rock'
+    METAL = 'metal'
+    ELECTRONIC = 'electronic'
+
 
 app = FastAPI()
 
 BANDS = [
     {
         "id" : 1,
-        "name" : "Band 1"
+        "name" : "Band 1",
+        "genre" : "Rock",
     },
     {
         "id" : 2,
-        "name" : "Band 2"
+        "name" : "Band 2",
+        "genre" : "Electronic",
     },
     {
         "id" : 3,
-        "name" : "Band 3"
+        "name" : "Band 3",
+        "genre" : "Metal",
     },
     {
         "id" : 4,
-        "name" : "Band 4"
+        "name" : "Band 4",
+        "genre" : "Rock",
     },
 ]
 
 
 
-@app.get('/')
+@app.get('/bands')
 def index():
     return BANDS
 
-@app.get('/{band_id}')
+@app.get('/bands/genre/{genre}')
+def band(genre : BandGenreChoices):
+    return [
+        b for b in BANDS if b["genre"].lower() == genre.value
+    ]
+
+@app.get('/bands/{band_id}')
 def band(band_id : int):
     band = next((b for b in BANDS if b["id"] == band_id), None)
     if band == None:
@@ -35,3 +52,5 @@ def band(band_id : int):
     else:
         return band
     
+    
+
